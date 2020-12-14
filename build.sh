483f66ba3625f65ab1a788cbbb941e02a347f6dd
neelesh-wayne <gi1603@wayne.edu>
2020-12-13 21:43:19 -0500

    Added build script

--- build.sh
+++ build.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 cd $TRAVIS_BUILD_DIR/
 sbt ++$TRAVIS_SCALA_VERSION package
+
+  - php PHPUnitTest/tests/CancelOrderKTest.php
+  - php PHPUnitTest/tests/countValuesTest.php
+  - php PHPUnitTest/tests/CreateDelivererAccountTest.php
+  - php PHPUnitTest/tests/CreateStudentAccountTest.php
+  - php PHPUnitTest/tests/DelivererEmailTest.php
+  - php PHPUnitTest/tests/delivererHasOrderTest.php
+  - php PHPUnitTest/tests/deliverertest.php
+  - php PHPUnitTest/tests/EditMenuTest.php
+  - php PHPUnitTest/tests/EmailTest.php
+  - php PHPUnitTest/tests/ExampleTest.php
+  - php PHPUnitTest/tests/LoginTest.php
+  - php PHPUnitTest/tests/MockTest.php
+  - php PHPUnitTest/tests/orderingPriceTest.php
+  - php PHPUnitTest/tests/orderingTest.php
+  - php PHPUnitTest/tests/PasswordTest.php
+  - php PHPUnitTest/tests/StudentEmailTest.php
+  - php PHPUnitTest/tests/UserTest.php
